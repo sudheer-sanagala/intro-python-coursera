97cf900dffc119cb76ea63199a1cbb835de3235d
sudheer-sanagala <get2sudheer@gmail.com>
2020-12-07 15:35:51 -0800

    add tuples

--- WK-04-Lists,Tuples/lists.py
+++ WK-04-Lists,Tuples/lists.py
@@ -1,7 +1,9 @@
+## List are created with square brackets [ ]. List are mutable, which means, the values of the list can be updated.
 
 list_1 = ['head', 'ears', 3, 'legs', 5.8]
 #print(type(list_1))
 print(list_1)
+#print(list_1.reverse)
 
 # add elements to the list. `append` always adds new element at the end
 list_1.append("Hello")
@@ -23,3 +25,27 @@ print(list_1)
 
 #list_1[10] = 'Sudheer' Index out of bounds
 #print(list_1)
+print("-------------------")
+print(list_1[0]) #first element
+print(list_1[:]) #all the elements
+print(list_1[2:4]) # from 3rd element to but not including 5th
+print(list_1[-1]) #last element
+print(list_1[1:-1]) #2nd element to but not including last
+
+# multiply a list, it can include integers and strings
+a = [1,2,3]
+print(a * 3)
+
+b = [4,5,"Hello"]
+print(b * 2)
+
+c = ['S','U']
+print(c*3)
+
+
+# Check element in the list
+
+
+# loop through a list
+for i in list_1:
+    print(i)
---
+++ WK-04-Lists,Tuples/tuples.py
@@ -0,0 +1,44 @@
+# These are immutable sequences of objects enclosed by parentheses ()
+# immutable means, once created a tuple, they cannot be modified
+
+tuple_1 = (3, 'bags', 'belt', 2020, 7.9)
+tuple_2 = (1,4,6,2)
+
+# Tuples are indexed like lists, so values can be extracted by specifying index
+print(tuple_1[1])
+print(tuple_1[0])
+print(tuple_1[2:-1])
+
+print('backpack' in tuple_1)
+print(len(tuple_1))
+print(max(tuple_2))
+
+
+# A dictionary is an unordered collection of key-value pairs enclosed by curly braces {}. 
+# Dictionaries are useful for storing information in pairs of ‘keys’ and ‘values’.
+dict_2 = {'fruits': ['apple','grape'],
+           10     : 'number of fruits',
+         'carrier': 'bag'}
+print(dict_2)
+
+print(dict_2['fruits'])
+print(dict_2[10])
+
+
+# append a value to a dict. It adds at the end
+dict_2['fruits'].append("banana")
+print(dict_2)
+
+# update a value to a dict
+dict_2['carrier'] = 'basket'
+print(dict_2)
+
+# add a new value to dict
+dict_2['status'] = 'not enough'
+print(dict_2)
+
+
+print('-------------------------------')
+print(dict_2.keys())
+print(dict_2.values())
+

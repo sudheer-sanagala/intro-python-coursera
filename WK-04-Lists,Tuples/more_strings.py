3643c25861a34ab72ebc697e05abe58864f8c7a7
sudheer-sanagala <get2sudheer@gmail.com>
2020-12-13 11:27:03 -0800

    more strings

--- WK-04-Lists,Tuples/more_strings.py
+++ WK-04-Lists,Tuples/more_strings.py
@@ -1,4 +1,24 @@
 """
+String operations
+len(string) Returns the length of the string
+for character in string Iterates over each character in the string
+if substring in string Checks whether the substring is part of the string
+string[i] Accesses the character at index i of the string, starting at zero
+string[i:j] Accesses the substring starting at index i, ending at index j-1. If i is omitted, it's 0 by default. If j is omitted, it's len(string) by default.
+
+String methods
+string.lower() / string.upper() Returns a copy of the string with all lower / upper case characters
+string.lstrip() / string.rstrip() / string.strip() Returns a copy of the string without left / right / left or right whitespace
+string.count(substring) Returns the number of times substring is present in the string
+string.isnumeric() Returns True if there are only numeric characters in the string. If not, returns False.
+string.isalpha() Returns True if there are only alphabetic characters in the string. If not, returns False.
+string.split() / string.split(delimiter) Returns a list of substrings that were separated by whitespace / delimiter
+string.replace(old, new) Returns a new string where all occurrences of old have been replaced by new.
+delimiter.join(list of strings) Returns a new string with all the strings joined by the delimiter 
+"""
+
+
+"""
 String Arithmatic
 """
 # multiply a string
@@ -44,8 +64,41 @@ print(fruit[1:5])
 print(fruit[:2]) #index starts from 0 and ends with (2 - 1) index
 print(fruit[2:]) #index starts from 2 and ends till end of string
 
-
-
-
-
 big_string = 'Fantastic Mr.Fox jumps on a rope'
+
+# Strip method - removes white spaces, tab spaces
+name = "SUdheer   "
+print(name.strip())
+print(name.upper())
+
+# returns the no of times a character or word occurs
+print(big_string.count("a"))
+print(big_string.count("an"))
+
+print(big_string.endswith("rope")) #True
+
+
+# Join method - used to concatenate wors with a string sperated by space. It uses list of strings
+join_var = " ".join(["This","is","a","senntence","joined","by","join","method"])
+print(join_var)
+
+join_var = "+".join(["This","is","a","senntence","joined","by","join","method"]) # join using + symbol
+print(join_var)
+
+# Split - This allows us to split a string into a list of strings.By default, it splits by any whitespace characters.
+#  You can also split by any other characters by passing a parameter.
+"This+is+a+senntence+joined+by+join+method"
+print(join_var.split("+")) # the above string is split by the character specified into a list of strings
+
+print("This is a senntence joined by join method".split())
+
+
+def initials(phrase):
+    words = phrase.split() # split into individual words
+    print(words)
+    result = ""
+    for word in words:
+        result += word[0] # extracts the first character of the string
+    return result
+
+print(initials("Universal Serial Bus"))

70a58d50d99d33aa5889a4a2f63defb02d04b733
sudheer-sanagala <get2sudheer@gmail.com>
2021-01-09 20:00:56 -0800

    more dictionary

--- WK-04-Lists,Tuples/dictionary.py
+++ WK-04-Lists,Tuples/dictionary.py
@@ -20,3 +20,25 @@ tech_dictionary["Python"] = "Most famous programming language in 2020"
 
 # Remove item
 tech_dictionary.pop("C#")
+
+file_counts = {"jpg":10, "txt":15, "csv":20}
+print(file_counts)
+print(file_counts["csv"])
+
+print("jpg" in file_counts) # check jpg exists in dictionary
+print("html" in file_counts) # html does not exists in dictionary
+
+# add new dictionary value
+file_counts["cfg"] = 8
+print(file_counts)
+
+# update an existing dictionary value
+# Before update - {'jpg': 10, 'txt': 15, 'csv': 20, 'cfg': 8}
+# After update - {'jpg': 10, 'txt': 15, 'csv': 10, 'cfg': 8}
+file_counts["csv"] = 10
+print(file_counts)
+
+# delete a key from dictionary
+#del file_counts["cfg2"] # if key does not exist it will return KeyError: 'cfg2'
+del file_counts["cfg"]
+print(file_counts)
